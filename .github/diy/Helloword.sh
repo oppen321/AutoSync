be025a0fa7073eb79c765f2bd2c72d176e5f98fd
oppen321 <zj18139624826@gmail.com>
2024-08-31 10:02:10 +0800

    Update Helloword.sh

--- .github/diy/Helloword.sh
+++ .github/diy/Helloword.sh
@@ -27,7 +27,6 @@ git clone --depth 1 https://github.com/immortalwrt/homeproxy luci-app-homeproxy
 git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass ./ ; rm -rf openwrt-packages
 git clone --depth 1 https://github.com/morytyann/OpenWrt-mihomo OpenWrt-mihomo && mv -n OpenWrt-mihomo/luci-app-mihomo ./ ; rm -rf OpenWrt-mihomo
 git clone --depth 1 https://github.com/morytyann/OpenWrt-mihomo OpenWrt-mihomo && mv -n OpenWrt-mihomo/mihomo ./ ; rm -rf OpenWrt-mihomo
-git clone --depth 1 https://github.com/sbwml/openwrt_helloworld openwrt_helloworld && mv -n openwrt_helloworld/patch-luci-app-passwall.patch ./ ; rm -rf openwrt_helloworld
 
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
