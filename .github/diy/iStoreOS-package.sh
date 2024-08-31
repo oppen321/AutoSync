27adf8a2c22cea0e36b2c0cd8682baa93899c28f
oppen321 <zj18139624826@gmail.com>
2024-08-31 23:14:30 +0800

    Update iStoreOS-package.sh

--- .github/diy/iStoreOS-package.sh
+++ .github/diy/iStoreOS-package.sh
@@ -14,12 +14,12 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-git clone --depth 1 -b main https://github.com/linkease/istore-packages
-git clone --depth 1 -b dev https://github.com/jjm2473/luci-app-diskman
-git clone --depth 1 -b dev4 https://github.com/jjm2473/OpenAppFilter 
-git clone --depth 1 -b master https://github.com/linkease/nas-packages
-git clone --depth 1 -b main https://github.com/linkease/nas-packages-luci
-git clone --depth 1 -b main https://github.com/jjm2473/openwrt-apps
+git clone --depth 1 -b main https://github.com/linkease/istore-packages istore-packages && mv -n istore-packages/* ./;rm -rf istore-packages
+git clone --depth 1 -b dev https://github.com/jjm2473/luci-app-diskman luci-app-diskman && mv -n luci-app-diskman/* ./;rm -rf luci-app-diskman
+git clone --depth 1 -b dev4 https://github.com/jjm2473/OpenAppFilter oaf && mv -n oaf/* ./;rm -rf oaf 
+git clone --depth 1 -b master https://github.com/linkease/nas-packages nas-packages && mv -n nas-packages/* ./;rm -rf nas-packages
+git clone --depth 1 -b main https://github.com/linkease/nas-packages-luci nas-packages-luci && mv -n nas-packages-luci/* ./;rm -rf nas-packages-luci
+git clone --depth 1 -b main https://github.com/jjm2473/openwrt-apps openwrt-apps && mv -n openwrt-apps/* ./;rm -rf openwrt-apps
 
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
