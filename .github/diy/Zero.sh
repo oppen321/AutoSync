0340b9d76c59d7d9f595a3b8e5eb1cf3072cd1bb
oppen321 <zj18139624826@gmail.com>
2024-10-25 22:56:51 +0800

    Update Zero.sh

--- .github/diy/Zero.sh
+++ .github/diy/Zero.sh
@@ -46,7 +46,7 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite
 git clone --depth=1 https://github.com/oppen321/luci-app-gpsysupgrade
 git clone --depth=1 https://github.com/kiddin9/luci-theme-edge
 git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus
-
+git clone --depth=1 https://github.com/kiddin9/kwrt-packages kwrt && mv -n kwrt/{wrtbwmon} ./ ; rm -rf kwrt
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
 -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
