3af36445351b34a04442c2daea7fd3a5d6b172f3
oppen321 <zj18139624826@gmail.com>
2024-10-25 20:59:24 +0800

    Update Zero.sh

--- .github/diy/Zero.sh
+++ .github/diy/Zero.sh
@@ -45,6 +45,7 @@ git clone --depth=1 https://github.com/pymumu/openwrt-smartdns
 git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite
 git clone --depth=1 https://github.com/ximiTech/msd_lite
 git clone --depth=1 https://github.com/oppen321/luci-app-gpsysupgrade
+git clone --depth=1 https://github.com/kiddin9/luci-theme-edge
 
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
