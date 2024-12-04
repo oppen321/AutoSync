1188b31a74ccbac20d1712986feb017b379178be
oppen321 <zj18139624826@gmail.com>
2024-12-04 10:14:42 +0800

    Update package.sh

--- .github/diy/package.sh
+++ .github/diy/package.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+function git_sparse_clone() {
+branch="$1" rurl="$2" localdir="$3" && shift 3
+git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+cd $localdir
+git sparse-checkout init --cone
+git sparse-checkout set $@
+mv -n $@ ../
+cd ..
+rm -rf $localdir
+}
 
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
+git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/luci && mv -n luci/applications/{luci-app-cpufreq,luci-app-diskman,luci-app-netdata,luci-app-ramfree,luci-app-socat,luci-app-syncdial,luci-app-usb-printer,luci-app-zerotier} ./ && rm -rf luci
+git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/package && mv -n package/net/{parted,socat,luci-app-syncdial,zerotier} ./ && rm -rf package
+git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/package && mv -n package/admin/{netdata} ./ && rm -rf package
+
+sed -i \
+-e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
+-e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
+*/Makefile
+sed -i 's/+libcap /+libcap +libcap-bin /' luci-app-openclash/Makefile
+
+rm -rf ./*/.git ./*/.gitattributes ./*/.svn ./*/.github ./*/.gitignore
+#find . -type f -name Makefile -exec sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' {} +
+exit 0
