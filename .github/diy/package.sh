69d5fea07ebc7c3ccb23b41c168da2fa107957fe
OPPEN321 <zj18139624826@gmail.com>
2025-02-09 01:42:14 +0800

    Update package.sh

--- .github/diy/package.sh
+++ .github/diy/package.sh
@@ -14,7 +14,7 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/luci && mv -n luci/applications/{luci-app-cpufreq,luci-app-diskman,luci-app-netdata,luci-app-ramfree,luci-app-socat,luci-app-syncdial,luci-app-airplay2,luci-app-usb-printer,luci-app-openvpn-server} ./ && rm -rf luci
+git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/luci && mv -n luci/applications/{luci-app-cpufreq,luci-app-diskman,luci-app-netdata,luci-app-ramfree,luci-app-socat,luci-app-syncdial,luci-app-usb-printer,luci-app-openvpn-server} ./ && rm -rf luci
 git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/packages && mv -n packages/net/socat ./ && rm -rf packages
 git clone --depth 1 -b openwrt-23.05 https://github.com/coolsnowwolf/luci && mv -n luci/applications/luci-app-zerotier ./ && rm -rf luci
 git clone --depth 1 -b master https://github.com/coolsnowwolf/packages && mv -n packages/net/zerotier ./ && rm -rf packages
