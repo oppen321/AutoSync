421b729a3cfe49ffa022286a3f5ec1ea5a9d6e65
OPPEN321 <zj18139624826@gmail.com>
2025-02-20 23:56:46 +0800

    Update package.sh

--- .github/diy/package.sh
+++ .github/diy/package.sh
@@ -25,7 +25,6 @@ git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/packages && 
 git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/packages && mv -n packages/utils/parted ./ && rm -rf packages
 git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/immortalwrt && mv -n immortalwrt/package/emortal/cpufreq ./ && rm -rf immortalwrt
 git clone --depth 1 -b main https://github.com/sbwml/openwrt_pkgs && mv -n openwrt_pkgs/{luci-app-ota,fw_download_tool,ddns-scripts-aliyun,bash-completion,luci-app-netspeedtest,speedtest-cli} ./ && rm -rf openwrt_pkgs
-git clone --depth 1 -b main https://github.com/siropboy/luci-app-bypass && mv -n luci-app-bypass ./ && rm -rf luci-app-bypass
 git clone --depth 1 -b main https://github.com/Lienol/openwrt-package && mv -n openwrt-package/luci-app-guest-wifi ./ && rm -rf openwrt-package
 git clone --depth 1 -b istoreos-24.10 https://github.com/istoreos/istoreos && mv -n istoreos/package/istoreos-files ./ && rm -rf istoreos
 git clone --depth 1 -b main https://github.com/linkease/nas-packages-luci && mv -n nas-packages-luci/luci/{luci-app-quickstart,luci-app-floatip,luci-app-linkease} ./ && rm -rf nas-packages-luci
