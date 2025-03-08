4529f22ff4bd91cd5728c46f9503e616b5efad2d
OPPEN321 <zj18139624826@gmail.com>
2025-03-09 01:28:14 +0800

    Update v24.10.sh

--- .github/diy/v24.10.sh
+++ .github/diy/v24.10.sh
@@ -28,6 +28,7 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-parentcontrol
 git clone --depth 1 https://github.com/sirpdboy/luci-app-eqosplus
 git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 git clone --depth 1 https://github.com/animegasan/luci-app-wolplus
+git clone --depth 1 https://github.com/lisaac/luci-app-diskman
 
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
