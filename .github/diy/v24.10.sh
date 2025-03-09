224749e9f963e8e77817e16b6e9e4aa0e62838d0
OPPEN321 <zj18139624826@gmail.com>
2025-03-09 11:42:47 +0800

    Update v24.10.sh

--- .github/diy/v24.10.sh
+++ .github/diy/v24.10.sh
@@ -30,7 +30,7 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 git clone --depth 1 https://github.com/lisaac/luci-app-diskman
 git clone --depth 1 -b main https://github.com/sirpdboy/sirpdboy-package && mv -n sirpdboy-package/luci-app-wolplus ./ && rm -rf sirpdboy-package
 git clone --depth 1 https://github.com/sbwml/openwrt_pkgs && mv -n openwrt_pkgs/{luci-app-netspeedtest,bash-completion,speedtest-cli} ./ && rm -rf openwrt_pkgs
-git clone --depth 1 https://github.com/immortalwrt/luci && mv -n luci/applications/{luci-app-cpufreq,luci-app-ramfree,luci-app-socat,luci-app-usb-printer,luci-app-zerotier} ./ && rm -rf luci
+git clone --depth 1 https://github.com/immortalwrt/luci && mv -n luci/applications/{luci-app-netdata,luci-app-cpufreq,luci-app-ramfree,luci-app-socat,luci-app-usb-printer,luci-app-zerotier} ./ && rm -rf luci
 git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/packages && mv -n packages/net/{socat,zerotier} ./ && rm -rf packages
 git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/immortalwrt && mv -n immortalwrt/package/emortal/cpufreq ./ && rm -rf immortalwrt
 
