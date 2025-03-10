4038d9c3adcd6e62a05c351e971314148d129d52
OPPEN321 <zj18139624826@gmail.com>
2025-03-10 21:58:39 +0800

    Update v24.10.sh

--- .github/diy/v24.10.sh
+++ .github/diy/v24.10.sh
@@ -28,6 +28,9 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-parentcontrol
 git clone --depth 1 https://github.com/sirpdboy/luci-app-eqosplus
 git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 git clone --depth 1 https://github.com/lisaac/luci-app-diskman
+git clone --depth 1 https://github.com/sbwml/luci-app-mentohust
+git clone --depth 1 https://github.com/pymumu/luci-app-smartdns
+git clone --depth 1 https://github.com/pymumu/openwrt-smartdns
 git clone --depth 1 -b main https://github.com/sbwml/luci-app-alist && mv -n luci-app-alist/{luci-app-alist,alist} ./ && rm -rf luci-app-alist
 git clone --depth 1 -b v5 https://github.com/sbwml/luci-app-mosdns && mv -n luci-app-mosdns/{luci-app-mosdns,mosdns,v2dat} ./ && rm -rf luci-app-mosdns
 git clone --depth 1 -b main https://github.com/sirpdboy/sirpdboy-package && mv -n sirpdboy-package/luci-app-wolplus ./ && rm -rf sirpdboy-package
