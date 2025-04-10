489a3a80d2b9a7f94d46fc47836740935b3447a6
OPPEN321 <zj18139624826@gmail.com>
2025-04-10 16:44:54 +0800

    Update build-packages.sh

--- .github/diy/build-packages.sh
+++ .github/diy/build-packages.sh
@@ -21,6 +21,7 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-eqosplus
 git clone --depth 1 https://github.com/sirpdboy/luci-app-parentcontrol
 git clone --depth 1 https://github.com/oppen321/luci-app-adguardhome
 git clone --depth 1 https://github.com/immortalwrt/homeproxy
+git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki && mv -n OpenWrt-nikki/{luci-app-nikki,nikki} ./ && rm -rf OpenWrt-nikki
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall && mv -n openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 && mv -n openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
 git clone --depth 1 -b master https://github.com/fw876/helloworld && mv -n helloworld/simple-obfs ./ && rm -rf helloworld
